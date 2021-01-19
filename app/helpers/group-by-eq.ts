b536108aa9588ee48386c7ab224f92a916be899c
Andrey Mikhaylov (lolmaus) <lolmaus@gmail.com>
2021-01-19 10:35:56 +0300

    Add caching to cards grouping for vertical collection of grid layout

--- app/helpers/group-by-eq.ts
+++ app/helpers/group-by-eq.ts
@@ -1,38 +1,74 @@
-import { helper } from '@ember/component/helper';
+import Helper from '@ember/component/helper';
 
-export function groupByEq<T>(
-  _pos: [],
-  {
-    items,
-    itemWidth,
-    padding,
-    width,
-  }: {
-    items: T[];
-    itemWidth: number;
-    padding: number;
-    width?: number;
-  }
-): T[][] {
-  if (!width) {
-    return [];
-  }
-
-  const count = Math.floor((width + padding) / (itemWidth + padding));
-
-  if (count < 1) {
-    return items.map((item) => [item]);
-  }
-
-  return items.reduce((result, item, index) => {
-    if (index % count === 0) {
-      result.push([]);
-    }
-
-    result[result.length - 1].push(item);
-
-    return result;
-  }, [] as T[][]);
+interface Cache<T> {
+  items: T[];
+  itemWidth: number;
+  padding: number;
+  width: number;
+  result: T[][];
 }
 
-export default helper(groupByEq);
+/**
+ * Note: This helper does not support mutating the items array!
+ **/
+
+export default class groupByHelper<T> extends Helper {
+  previousValues?: Cache<T>;
+
+  compute(
+    _pos: [],
+    {
+      items,
+      itemWidth,
+      padding,
+      width,
+    }: {
+      items: T[];
+      itemWidth: number;
+      padding: number;
+      width?: number;
+    }
+  ): T[][] {
+    // If all arguments are the same, return cached result
+    if (
+      !!this.previousValues &&
+      items === this.previousValues.items &&
+      itemWidth === this.previousValues.itemWidth &&
+      padding === this.previousValues.padding &&
+      width === this.previousValues.width
+    ) {
+      return this.previousValues?.result;
+    }
+
+    if (!width) {
+      return [];
+    }
+
+    const count = Math.floor((width + padding) / (itemWidth + padding));
+
+    if (count < 1) {
+      return items.map((item) => [item]);
+    }
+
+    const result = items.reduce((result, item, index) => {
+      if (index % count === 0) {
+        result.push([]);
+      }
+
+      result[result.length - 1].push(item);
+
+      return result;
+    }, [] as T[][]);
+
+    // Cache the new result
+    this.previousValues = {
+      items,
+      itemWidth,
+      padding,
+      width,
+      result,
+    };
+
+    return result;
+  }
+}
